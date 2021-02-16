07a0ed73e43809310b82520dd21f132e2997b8a0
priyal-babel <priyal.babel@somaiya.edu>
2021-02-17 00:17:47 +0530

    Added more data

---
+++ Database/inclusions.sql
@@ -0,0 +1,7 @@
+USE hotelmanagement;
+INSERT INTO inclusions values(
+"Premium",1,0,1,0,1,0,1,0);
+INSERT INTO inclusions values(
+"Deluxe",1,1,1,0,1,1,1,0);
+INSERT INTO inclusions values(
+"Luxury",1,1,1,1,1,1,1,1);
