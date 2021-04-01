df93bbcbff6c2c524f2d7471c1b5e0df1e347851
Pinaki Jayakar <pjayakar01@gmail.com>
2021-04-02 00:17:35 +0530

    Debugging booking connection

--- bookingconnection.php
+++ bookingconnection.php
@@ -4,7 +4,9 @@ $y=$_POST['checkout'];
 $z=$_POST['rooms'];
 $a=$_POST['type'];
 $b=$_POST['email'];
-
+$d="Premium";
+$e="Deluxe";
+$f="Luxury";
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -16,8 +18,24 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
-$sql = "INSERT INTO booking_data (checkin, checkout, no_of_rooms, room_type, email)
- VALUES ('$x','$y','$z','$a','$b')";
+
+if($a = $e){
+  $sql = "UPDATE booking_date
+  SET premium_availability = premium_availability - $z
+  WHERE date_of_booking BETWEEN '$x' AND '$y'";
+}
+if($a = $e){
+  $sql = "UPDATE booking_date
+  SET deluxe_availability = deluxe_availability - $z
+  WHERE date_of_booking BETWEEN '$x' AND '$y'";
+}
+if($a = $f)
+{
+  $sql = "UPDATE booking_date
+  SET luxury_availability = luxury_availability - $z
+  WHERE date_of_booking BETWEEN '$x' AND '$y'";
+}
+
 if ($conn->query($sql) === TRUE){
 	echo "\nNew record created successfully";
   header("Location: payment.php");
