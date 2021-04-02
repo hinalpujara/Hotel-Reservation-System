33c6acdc468f92cc3d8b396116d1b7f42a1ba7db
Hinal Pujara <hinal.pujara@somaiya.edu>
2021-04-02 22:26:06 +0530

    added try.html for home page

--- index.php
+++ index.php
@@ -1,47 +1,88 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Home-The Victoria</title>
-    <style type="text/css">
-        li{
-            font-size: 20px;
-        }
-        header{
-          position: fixed;
-          width: 100%;
-        }
-    </style>
-      </head>
-      <body>
-      <?php include "navbar.php"; ?>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <title>Home-The Victoria</title>
+  <style type="text/css">
+    li {
+      font-size: 20px;
+    }
+
+    header {
+      position: fixed;
+      width: 100%;
+      z-index: 100;
+    }
+    }
+  </style>
+</head>
+
+<body>
+  <?php include "navbar.php"; ?>
 
   <!-- welcome!! -->
-  <div class="sections_group">
+
+  <div class="sections_group" style="padding-top:70px;">
     <table>
       <tr>
         <td>
           <div class="entry-content" itemprop="mainContentOfPage">
-            <div class="section mcb-section   "  style="padding-top:0px; padding-bottom:60px; background-color:" ><div class="section_wrapper mcb-section-inner"><div class="wrap mcb-wrap one  valign-top clearfix" style="" ><div class="mcb-wrap-inner"><div class="column mcb-column one-second column_image "><div class="image_frame image_item no_link scale-with-grid no_border" ><div class="image_wrapper"><img class="scale-with-grid" src="udaipur3.jpg" alt="home_hotel2_gallery7" width="800" height="500" /></div>
-            </div>
+            <div class="section mcb-section   " style="padding-top:0px; padding-bottom:30px; background-color:">
+              <div class="section_wrapper mcb-section-inner">
+                <div class="wrap mcb-wrap one  valign-top clearfix">
+                  <div class="mcb-wrap-inner">
+                    <div class="column mcb-column one-second column_image ">
+                      <div class="image_frame image_item no_link scale-with-grid no_border">
+                        <div class="image_wrapper">
+                          <img class="scale-with-grid" src="udaipur3.jpg" alt="home_hotel2_gallery7">
+                        </div>
+                      </div>
         </td>
         <td>
-          <div class="column mcb-column one-second column_column  column-margin-"><div class="column_attr clearfix align_left"    style=" padding:20px;"><h5>Experience the best Hospitality</h5>
-            <h2><b>Welcome to The Victoria</b></h2>
+          <div class="column mcb-column one-second column_column  column-margin-">
+            <div class="column_attr clearfix align_left" style=" padding:20px;">
+              <h5>Experience the best Hospitality</h5>
+              <h2><b>Welcome to The Victoria</b></h2>
 
-            <p>With a renowned presence in cities like Jaipur, Udaipur, Nathdwara and Kumbhalgarh, the Victoria hotels have found their popularity in the land of Rajputana, Rajasthan. </p>
-            <p><b>Victoria hotels provide an affordable and budgeted stay to all its guests, keeping up and maintaining with the comfort and quality of services that it offers. </b></p>
+              <p>With a renowned presence in cities like Jaipur, Udaipur, Nathdwara and Kumbhalgarh, the Victoria hotels have found their popularity in the land of Rajputana, Rajasthan. </p>
+              <p><b>Victoria hotels provide an affordable and budgeted stay to all its guests, keeping up and maintaining with the comfort and quality of services that it offers. </b></p>
         </td>
       </tr>
     </table>
+  </div>
+  <div style="text-align: center;">
+    <h1>
+      Amenities
+    </h1>
+  </div>
 
+  <div class="section mcb-section dark  " style="padding-top:40px; padding-bottom:25px; background-color:#ffffff; background-image:url(http://hopp-inn.com/wp-content/uploads/2015/11/home_hotel2_subheader1.jpg); background-repeat:no-repeat; background-position:center; background-attachment:; background-size:; -webkit-background-size:">
+    <div class="section_wrapper mcb-section-inner">
+      <div class="wrap mcb-wrap one  valign-top clearfix">
+        <div class="mcb-wrap-inner">
+          <div class="column mcb-column one-sixth column_placeholder">
+            <div class="placeholder">&nbsp;</div>
+          </div>
+          <div class="column mcb-column two-third column_column  column-margin-0px">
+            <div class="column_attr clearfix align_center">
+              <h2 style="color: white; text-align: center;">The rooms are well equipped and host an ambience of modern comforts, modern amenities and ensure a delightful experience throughout your stay.</h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+  <br>
+  <br>
+  
+  
 
 
 
@@ -50,5 +91,6 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
