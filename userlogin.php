43a8af8e823ea912788ec9de863fdc8f55ea21ea
Pinaki Jayakar <pjayakar01@gmail.com>
2021-04-08 20:43:44 +0530

    Session Debugging

--- userlogin.php
+++ userlogin.php
@@ -11,20 +11,92 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <style type="text/css">
-        header{
-          position: fixed;
-          width: 100%;
-          z-index: 100;
+        .bkg-img{
+            background-image: url("udr.jpg");
+            /*margin-top: 0;*/
+            font-size: 50px;
+            color: white;
+            text-align: left;
+            font-family: "Georgia",Serif;
+            font-weight: bold;
+            align-content: bottom;
+            padding-top: 425px;
+            padding-left: 50px;
+            text-shadow: 0 5px 10px black;
         }
         li{
             font-size: 20px;
+        }
+        .mycustom-jumbotron {
+              height: 600px;
+              width: 500px;
+              background-color: #FADBD8
+            }
+        .custom-btn{
+            background-color: #FADBD8;
+            border-width: 1px;
+            border-color: black;
+
+        }
+        .loc{
+            background: transparent;
+            border: 0px;
+        }
+        .img-size{
+            width: 70px;
+            height: 80px;
+        }
+        .image-card {
+            padding-left: -15px !important;
+            margin-left:-80px !important;
+}
+        .map-block{
+            display: inline-block;
         }
     </style>
     <title>The Victoria</title>
       </head>
       <body>
-      <?php include "navbar.php"; ?>
-
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #FADBD8">
+        <a class="navbar-brand" href="#">
+          <img src="logo.png" width="200" height="50" class="d-inline-block align-center" alt="">
+        </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active pl-3">
+            <a class="nav-link" href="index.php"><b>Home</b> <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item pl-3">
+            <a class="nav-link" href="#" style="color: black;"><b>About</b> </span></a>
+          </li>
+          <li class="nav-item pl-3">
+            <a class="nav-link" href="rooms.php" style="color: black;"><b>Rooms</b> </span></a>
+          </li>
+          <li class="nav-item pl-3">
+            <a class="nav-link" href="booking.php" style="color: black;"><b>Book</b></a>
+          </li>
+          <li class="nav-item pl-3">
+            <a class="nav-link" href="contactus.php" style="color: black;"><b>Contact Us</b></a>
+          </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <b>Login/Register</b>
+              </a>
+              <div class="dropdown-menu" aria-labelled by="navbarDropdown">
+                <a class="dropdown-item" href="userlogin.php">User Login</a>
+                <a class="dropdown-item" href="adminlog.php">Admin Login</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="register.php">User Registration</a>
+              </div>
+            </li>
+        </ul>
+      </div>
+    </nav>
 	<form action="user_login_validation.php" method="post">
         <div class="login-box">
             <h1>USER LOGIN</h1>
